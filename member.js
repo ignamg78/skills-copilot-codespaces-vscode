e2e047ddfb2c9694c95ad39f97234f65d551eab8
ignamg78 <ignaciomg78@hotmail.com>
2025-01-16 17:43:19 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    let skills = ['HTML', 'CSS', 'JavaScript', 'React', 'Node', 'Python'];
+    let member = {
+        name: 'John',
+        age: 25,
+        skills: skills
+    };
+
+    return member;
+}
